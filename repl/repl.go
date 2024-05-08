43ce1e60c38cf29d38f11405db468114328ffa1d
tristanlok <tristanlok@outlook.com>
2024-05-08 20:50:21 +0000

    Created the scanner

--- repl/repl.go
+++ repl/repl.go
@@ -1,9 +1,21 @@
 package repl
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"gosh/scanner"
+	"os"
 )
 
 func Start () {
-    fmt.Println("Hello!!!")
+    // Initiate variable and scanner
+    var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+    var line string
+
+    // Beginning of Loop
+    for true {
+        fmt.Print("> ")
+        scan.ScanLine(scanner, &line)
+        scan.GetArgs(&line)
+    }
 }
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,3 +1,17 @@
-package gosh
+package scan
 
+import (
+	"bufio"
+	"fmt"
+)
 
+func ScanLine (scanner *bufio.Scanner, line *string) {
+    scanner.Scan()
+    *line = scanner.Text()
+}
+
+func GetArgs (line *string) {
+    for i := 0; i < len(*line); i++ {
+        fmt.Println((*line)[i])
+    }
+}
